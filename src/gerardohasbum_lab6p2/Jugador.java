ac4bcea6f131379dcb6e7d5a4d19932e287cbe79
GerardoHasbum <ghasbumgo@gmail.com>
2024-02-23 14:42:27 -0600

    Creacion de clases Equipo y Jugador

---
+++ src/gerardohasbum_lab6p2/Jugador.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gerardohasbum_lab6p2;
+
+/**
+ *
+ * @author ghasb
+ */
+public class Jugador {
+
+    String nombre, posicion;
+    int edad;
+
+    public Jugador() {
+    }
+
+    public Jugador(String nombre, String posicion, int edad) {
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNombre: " + nombre + "\nPosicion: " + posicion + "\nEdad: " + edad + "\n";
+    }
+
+}
